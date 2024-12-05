0ab7f86590b2847d6f3ef398c332329f377670ef
Stan Smith <choicedesign@gmail.com>
2024-12-04 22:16:16 -0700

    solve(day 1,part 1): ⭐️

--- day-01.ts
+++ day-01.ts
@@ -1,4 +1,3 @@
-// import fp from "lodash/fp"
 import _ from "lodash";
 import { getInput } from "./utils.ts";
 
@@ -10,17 +9,32 @@ export default {
 };
 
 function _getInput(isTest?: boolean) {
-	return (
-		getInput({ day: "01" }, isTest)
-			// .flatMap((locationIds) => locationIds.split(" "))
-			.map((locationIds) => locationIds.split(" ").filter(_.identity))
+	const input = getInput({ day: "01" }, isTest);
+	return input.map((locationIds) =>
+		locationIds
+			.split(" ")
 			.filter(_.identity)
+			.map((locationIds) => Number.parseInt(locationIds)),
 	);
 }
 
+// @see https://adventofcode.com/2024/day/1
 function part1() {
-	const input = _getInput(true);
-	console.log(input);
+	// const input = _getInput(true);
+	const input = _getInput();
+	// console.log(input);
+
+	const listLeft = _.sortBy(input.map((locationIds) => locationIds[0]));
+	const listRight = _.sortBy(input.map((locationIds) => locationIds[1]));
+	console.log({ listLeft, listRight });
+
+	const distances = listLeft.map((leftLocationId: number, index: number) => {
+		const rightLocationId = listRight[index];
+		return Math.abs(leftLocationId - rightLocationId);
+	});
+	console.log({ distances });
+
+	return _.sum(distances);
 }
 
 function part2() {
