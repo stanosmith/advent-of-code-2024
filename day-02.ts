7b3e0d1d6ee0917634610589ba352a2333da6085
Stan Smith <choicedesign@gmail.com>
2024-12-04 23:57:19 -0700

    solve(day 2,part one): ⭐️

--- day-02.ts
+++ day-02.ts
@@ -18,15 +18,60 @@ function _getInput(isTest?: boolean) {
 	);
 }
 
+// A report only counts as safe if both of the following are true:
+//     - The levels are either all increasing or all decreasing.
+//     - Any two adjacent levels differ by at least one and at most three.
 // @see https://adventofcode.com/2024/day/2
 function partOne() {
-	const input = _getInput(true);
-	// const input = _getInput();
-	console.log(input);
+	// const reports = _getInput(true);
+	const reports = _getInput();
+	console.log(reports);
+
+	const safeReports = reports.filter((report) => {
+		const increaseOrDecrease = (increase?: boolean) => {
+			return (level: number, index: number) => {
+				const nextLevel = report[index + 1];
+
+				if (typeof nextLevel === "undefined") return true;
+
+				return increase ? level < nextLevel : level > nextLevel;
+			};
+		};
+		const withinRange = (level: number, index: number) => {
+			const nextLevel = report[index + 1];
+			if (typeof nextLevel === "undefined") return true;
+
+			const diff = Math.abs(level - nextLevel);
+
+			return diff === 1 || diff === 2 || diff === 3;
+		};
+
+		const increasing = report.filter(increaseOrDecrease(true));
+		const allIncreasing = increasing.length === report.length;
+
+		const decreasing = report.filter(increaseOrDecrease());
+		const allDecreasing = decreasing.length === report.length;
+
+		const diffWithinRange = report.filter(withinRange);
+		const allDiffWithinRange = diffWithinRange.length === report.length;
+
+		console.table({
+			// increasing,
+			// decreasing,
+			allIncreasing,
+			allDecreasing,
+			// diffWithinRange,
+			allDiffWithinRange,
+		});
+
+		return (allIncreasing || allDecreasing) && allDiffWithinRange;
+	});
+
+	return safeReports.length;
 }
 
 function partTwo() {
-	const input = _getInput(true);
-	// const input = _getInput();
-	console.log(input);
+	const reports = _getInput(true);
+	// const reports = _getInput();
+	console.log(reports);
 }
