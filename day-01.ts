2f37fe2737ee3ee070224ab311cb983c66a3fe30
Stan Smith <choicedesign@gmail.com>
2024-12-04 22:45:43 -0700

    solve(day 1,part two): ⭐️⭐️

--- day-01.ts
+++ day-01.ts
@@ -18,14 +18,22 @@ function _getInput(isTest?: boolean) {
 	);
 }
 
+function getLeftRightLists(input: number[][], sort?: boolean) {
+	const listLeft = input.map((locationIds) => locationIds[0]);
+	const listRight = input.map((locationIds) => locationIds[1]);
+	return {
+		listLeft: sort ? _.sortBy(listLeft) : listLeft,
+		listRight: sort ? _.sortBy(listRight) : listRight,
+	};
+}
+
 // @see https://adventofcode.com/2024/day/1
 function partOne() {
 	// const input = _getInput(true);
 	const input = _getInput();
 	// console.log(input);
 
-	const listLeft = _.sortBy(input.map((locationIds) => locationIds[0]));
-	const listRight = _.sortBy(input.map((locationIds) => locationIds[1]));
+	const { listLeft, listRight } = getLeftRightLists(input, true);
 	console.log({ listLeft, listRight });
 
 	const distances = listLeft.map((leftLocationId: number, index: number) => {
@@ -37,7 +45,24 @@ function partOne() {
 	return _.sum(distances);
 }
 
+// Figure out exactly how often each number from the left list appears in the right list.
+// Calculate a total "similarity score" by adding up each number in the left list after
+// multiplying it by the number of times that number appears in the right list.
 function partTwo() {
-	const input = _getInput(true);
+	// const input = _getInput(true);
+	const input = _getInput();
 	console.log(input);
+
+	const { listLeft, listRight } = getLeftRightLists(input);
+	console.log({ listLeft, listRight });
+
+	const instances = listLeft.map((leftLocationId: number) => {
+		const matches = listRight.filter(
+			(locationId: number) => locationId === leftLocationId,
+		).length;
+		return leftLocationId * matches;
+	});
+	console.log({ instances });
+
+	return _.sum(instances);
 }
