473572e094242b03dec97dfd1602110fcd58bba4
Stan Smith <choicedesign@gmail.com>
2024-12-04 21:26:35 -0700

    day 1: scaffold day 1 module and input utility module

--- main.ts
+++ main.ts
@@ -1,8 +1,7 @@
-export function add(a: number, b: number): number {
-	return a + b;
-}
-
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-	console.log("Add 2 + 3 =", add(2, 3));
-}
+// e.g. `if (import.meta.main)`
+
+import day01 from "./day-01.ts";
+
+day01.solve.part1();
+day01.solve.part2();
