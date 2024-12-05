d43e710a4c0543df933d9157dc66e6bb72e61c80
Stan Smith <stan@df.com>
2024-12-05 11:29:57 -0700

    scaffold day 3 module and input

--- main.ts
+++ main.ts
@@ -2,7 +2,8 @@
 // e.g. `if (import.meta.main)`
 
 // import day01 from "./day-01.ts";
-import day02 from "./day-02.ts";
+// import day02 from "./day-02.ts";
+import day03 from "./day-03.ts";
 
 // --- Day 1: Historian Hysteria ---
 // console.log("Day 1 - Part One: ", day01.solve.partOne());
@@ -10,4 +11,8 @@ import day02 from "./day-02.ts";
 //
 // --- Day 2: Red-Nosed Reports ---
 // console.log("Day 2 - Part One: ", day02.solve.partOne());
-console.log("Day 2 - Part Two: ", day02.solve.partTwo());
+// console.log("Day 2 - Part Two: ", day02.solve.partTwo());
+//
+// --- Day 3: Red-Nosed Reports ---
+console.log("Day 3 - Part One: ", day03.solve.partOne());
+// console.log("Day 3 - Part Two: ", day03.solve.partTwo());
