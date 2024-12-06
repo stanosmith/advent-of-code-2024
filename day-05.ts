f31c72f0a4655a5cc0fb9a5c839ed5d2a5812b07
Stan Smith <stan@df.com>
2024-12-05 20:28:10 -0700

    scaffold days 5 module and input

---
+++ day-05.ts
@@ -0,0 +1,26 @@
+import _ from "lodash";
+import { getInput } from "./utils.ts";
+
+export default {
+	solve: {
+		partOne,
+		partTwo,
+	},
+};
+
+// @see https://adventofcode.com/2024/day/4
+function _getInput(isTest?: boolean) {
+	return getInput({ day: "05" }, isTest);
+}
+
+function partOne() {
+	const input = _getInput(true);
+	// const input = _getInput();
+	console.log(input);
+}
+
+function partTwo() {
+	const input = _getInput(true);
+	// const input = _getInput();
+	console.log(input);
+}
--- main.ts
+++ main.ts
@@ -5,6 +5,7 @@
 // import day02 from "./day-02.ts";
 // import day03 from "./day-03.ts";
 import day04 from "./day-04.ts";
+// import day05 from "./day-05.ts";
 
 // --- Day 1: Historian Hysteria ---
 // console.log("Day 1 - Part One: ", day01.solve.partOne());
@@ -14,10 +15,14 @@ import day04 from "./day-04.ts";
 // console.log("Day 2 - Part One: ", day02.solve.partOne());
 // console.log("Day 2 - Part Two: ", day02.solve.partTwo());
 //
-// --- Day 3: Red-Nosed Reports ---
+// --- Day 3: Mull It Over ---
 // console.log("Day 3 - Part One: ", day03.solve.partOne());
 // console.log("Day 3 - Part Two: ", day03.solve.partTwo());
 //
-// --- Day 4: Red-Nosed Reports ---
+// --- Day 4: Ceres Search ---
 console.log("Day 4 - Part One: ", day04.solve.partOne());
 // console.log("Day 4 - Part Two: ", day04.solve.partTwo());
+//
+// --- Day 5: Print Queue ---
+// console.log("Day 5 - Part One: ", day05.solve.partOne());
+// console.log("Day 5 - Part Two: ", day05.solve.partTwo());
